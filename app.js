596058653767952567a5e7c9760b2387b0a8f174
ebuczek525 <ebuczek@stevens.edu>
2020-05-03 17:42:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
 var JiraClient = require("jira-connector");
+const database = require('./database.js');
+
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+
+
+ database.connect("users", (success) => {
+      if(success){
+          console.log("=Database Connected");
+      }
+      else {
+          console.log("Database Connection Failed")
+      }
+})
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    goal: Number,
+    current_points: Number
+});
+
+const user = mongoose.model('users', userSchema);
 
 var clientId = '1057370358868.1070775925077';
 var clientSecret = '8b70616e59ebc57bd03d6a005962731e';
@@ -11,6 +34,12 @@ var app = express();
 app.use(bodyParser.json({limit: '256mb'}));
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 const PORT=3000;
 
 app.listen(PORT, function () {
@@ -19,7 +48,7 @@ app.listen(PORT, function () {
 });
 
 var jira = new JiraClient({
-    host: "jenjinstudios.atlassian.net",
+    host: "stevens-345-bot.atlassian.net",
     basic_auth: {
       email: "ebuczek@stevens.edu",
       api_token: "zEcjUpuhfLMkEcb1iHYvA47A"
@@ -31,8 +60,9 @@ app.get('/', function(req, res) {
 });
 
 app.get('/jira', async function(req, res) {
-    const issue = await jira.issue.getIssue({ issueKey: "BOT-1" });
-    res.send(issue);
+    //const issue = await jira.issue.getIssue({ issueKey: 'BOT-1' });
+    const board = await jira.board.getIssuesForBoard({ boardId: "1" })
+    res.send(board);
 });
 
 app.get('/oauth', function(req, res) {
@@ -58,15 +88,75 @@ app.get('/oauth', function(req, res) {
     }
 });
 
-app.post('/myprod', function(req, res) {
+app.post('/setgoal', async function(req,res) {
+
+    console.log(req.body.user_name);
+    console.log(req.body.text);
+
+    const sentData = new user({
+        username: req.body.user_name,
+        goal: req.body.text,
+        current_points: 0
+      });
+    
+    console.log('got to post');
+
+    sentData.save()
+        .then((item) => {
+          console.log('test saved to database');
+          res.sendStatus(200);
+        })
+        .catch((err) => {
+          console.log('errored out');
+          res.status(400);
+          res.send('got here db');
+        });
+    console.log('Bottom of server post');
+
+    res.send({
+
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": ":muscle: Hey " + req.body.user_name + "! Your goal has been set! :muscle:"
+                }
+            }
+        ]
+    });
+});
+
+app.post('/myprod', async function(req, res) {
 
     console.log(req.body);
-    
+
     user_name = req.body.user_name;
-    goal = '10 pts/Week';
-    task_avg = '5 points'
-    task_list = 'BOT-1';
-    pace = '5 pts/Week';
+    goal = JSON.stringify(await user.findOne({ username: user_name }, 'goal', function (err, user) {})).split('"goal":')[1].replace('}', '');
+
+    const task_list = await jira.board.getIssuesForBacklog({ boardId : 1 });
+
+    test1 = Object.values(task_list);
+    test2 = Object.values(test1)[4];
+
+    let tasks = [];
+
+    let storyPoints = [];
+
+    for(i = 0; i < test2.length; i++) {
+
+        values = Object.values(test2)[i];
+        fields = Object.values(values)[4];
+        taskName = ' ' + Object.values(fields)[48];
+        sPoints = ' ' + fields.customfield_10026 + ' points';
+      
+        tasks.push(taskName);
+        storyPoints.push(sPoints);
+      
+        var newArray = tasks.map((e, i) => `${e}:${storyPoints[i]}`);  
+    }
+
+    result = JSON.stringify(newArray).replace(/"/g,'').replace('[', ' ').replace(']', ' ');
 
     res.send({
 
@@ -89,35 +179,26 @@ app.post('/myprod', function(req, res) {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "*Average Task Value:*\n" + task_avg
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Available Tasks:*\n" + task_list
-                }
-            },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*Suggested Pace:*\n" + pace
+                    "text": "*Available Tasks:*\n" + result
                 }
             }
         ]
     });
 });
 
-app.post('/reminder', function(req, res) {
+app.post('/reminder', async function(req, res) {
 
     console.log(req.body);
 
+    issue_id = req.body.text;
+
+    const issue = await jira.issue.getIssue({ issueKey: issue_id });
+
     user_name = req.body.user_name;
-    link = 'https://stevens-345-bot.atlassian.net/secure/RapidBoard.jspa?rapidView=1&modal=detail&selectedIssue=BOT-1';
-    story_points = '5'
-    due_date = '1 week';
+    task_name = issue.fields.summary;
+    link = 'https://stevens-345-bot.atlassian.net/secure/RapidBoard.jspa?rapidView=1&modal=detail&selectedIssue=' + issue_id;
+    story_points = issue.fields.customfield_10026;
+    due_date = issue.fields.duedate;
 
     res.send({
 
@@ -133,7 +214,7 @@ app.post('/reminder', function(req, res) {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "*Task:*\n<" + link + ">"
+                    "text": "*Task:*\n" + task_name + "\n<" + link + ">"
                 }
             },
             {
@@ -154,14 +235,33 @@ app.post('/reminder', function(req, res) {
     });
 });
 
-app.post('/complete', function(req, res) {
+app.post('/complete', async function(req, res) {
 
     console.log(req.body);
 
+    issue_id = req.body.text;
+
+    const issue = await jira.issue.getIssue({ issueKey: issue_id });
+
     user_name = req.body.user_name;
-    link = 'https://stevens-345-bot.atlassian.net/secure/RapidBoard.jspa?rapidView=1&modal=detail&selectedIssue=BOT-1';
-    user_points = '10'
-    goal_progress = '5/10 points';
+    task_name = issue.fields.summary;
+    link = 'https://stevens-345-bot.atlassian.net/secure/RapidBoard.jspa?rapidView=1&modal=detail&selectedIssue=' + issue_id;
+    user_points = parseInt(JSON.stringify(await user.findOne({ username: user_name }, 'current_points', function (err, user) {})).split('"current_points":')[1].replace('}', '')) + parseInt(issue.fields.customfield_10026);
+    user.findOneAndUpdate(
+        {username: user_name}, 
+        {
+          current_points: user_points,
+        },
+        function(err, result) {
+          if (err) {
+            res.send(err);
+            console.log(err, result);
+          } else {
+            console.log(err, result);
+          }
+        }
+      );
+    goal_progress = parseInt(JSON.stringify(await user.findOne({ username: user_name }, 'current_points', function (err, user) {})).split('"current_points":')[1].replace('}', '')) / parseInt(JSON.stringify(await user.findOne({ username: user_name }, 'goal', function (err, user) {})).split('"goal":')[1].replace('}', '')) * 100 + '%';
 
     res.send({
 
@@ -177,14 +277,21 @@ app.post('/complete', function(req, res) {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "*Task:*\n<" + link + ">"
+                    "text": "*Task:*\n" + task_name + "\n<" + link + ">"
                 }
             },
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": "*Current User Points:*\n" + user_points + " (+5)"
+                    "text": "*Current User Points:*\n" + parseInt(JSON.stringify(await user.findOne({ username: user_name }, 'current_points', function (err, user) {})).split('"current_points":')[1].replace('}', ''))
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*Delta User Points:*\n" + "+" + issue.fields.customfield_10026
                 }
             },
             {
